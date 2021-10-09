62bd281c9dbb093870d3bf306a3545b5b090545b
Susmitha <susmithakundukulam@gmail.com>
2021-10-09 22:49:54 +0530

    api url added in controller

--- src/main/java/com/spec/spec/Controller/EmployeeController.java
+++ src/main/java/com/spec/spec/Controller/EmployeeController.java
@@ -14,24 +14,34 @@ import com.spec.spec.dto.EmployeeDto;
 @RequestMapping("employee")
 public interface EmployeeController {
 	
+	//http://localhost:8080/spec/employee/getAllEmployees
 	@PostMapping("getAllEmployees")
 	public ResponseEntity getAllEmployees() ;
 	
+	//http://localhost:8080/spec/employee/getAllEmployeesQuery
 	@PostMapping("getAllEmployeesQuery")
 	public ResponseEntity getAllEmployeesQuery() ;
 	
+	//http://localhost:8080/spec/employee/getAllEmployeesQuery1
 	@PostMapping("getAllEmployeesQuery1")
 	public ResponseEntity getAllEmployeesQuery1() ;
 	
+	//http://localhost:8080/spec/employee/getAllEmployeesQuery2
+	//{"id" : 2,"firstName" : "Susmitha","lastName" : "k"}
 	@PostMapping("getAllEmployeesQuery2")
 	public ResponseEntity getAllEmployeesQuery2(@RequestBody EmployeeDto empDto) ;
 	
+	//http://localhost:8080/spec/employee/getAllEmployeesQuery3
+	//{"id" : 2,"firstName" : "Susmitha","lastName" : "k"}
 	@PostMapping("getAllEmployeesQuery3")
 	public ResponseEntity getAllEmployeesQuery3(@RequestBody EmployeeDto empDto) ;
 	
+	//http://localhost:8080/spec/employee/getAllEmployeesByName
 	@PostMapping("getAllEmployeesByName")
 	public ResponseEntity getAllEmployeesByName() ;
 	
+	
+	//http://localhost:8080/spec/employee/getAllEmployeesSpec
 	@PostMapping("getAllEmployeesSpec")
 	public ResponseEntity getAllEmployeesSpec() ;
 }
