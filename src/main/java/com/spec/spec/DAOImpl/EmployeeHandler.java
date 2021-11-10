8b9daf187717a8a141108bb7f2db2e4791af827e
Susmitha <susmithakundukulam@gmail.com>
2021-11-10 13:38:13 +0530

    JPA specification for inner join

--- src/main/java/com/spec/spec/DAOImpl/EmployeeHandler.java
+++ src/main/java/com/spec/spec/DAOImpl/EmployeeHandler.java
@@ -54,4 +54,59 @@ public class EmployeeHandler {
 		
 		return empList;		
 	}
+	
+	public List<Employee> findAllBySpecjRoot(String firstName, int id){
+		List<EmployeeDto> empDtoList = new ArrayList<EmployeeDto>();
+		List<Employee> empList = new ArrayList<Employee>();
+
+		List<Specifications> specifications = new ArrayList<Specifications>();
+		 
+		employeeDAO.findAll(Specification.where((root, query, builder)->{
+			if(id != 0) {
+				specifications.add(new Specifications("id", id , "=", root, "or"));
+			}
+			if(firstName != null) {
+				specifications.add(new Specifications("firstName", "%"+firstName , "%", root, "or"));
+			}
+			// Table join example
+			//final Join<Employee, EmployeeDetailEntity> empJoin = querySpecification.makeJoin(root, new Employee(), new EmployeeDetailEntity(), "id", JoinType.INNER);
+			query.orderBy(builder.desc(root.get("id"))); //asc or desc based on id if required
+			return builder.and(querySpecification.writeJRootsAndCondition(specifications, builder));
+
+		})).forEach((emp)->{
+			empList.add(emp);
+		});
+		
+		return empList;		
+	}
+	
+	//SAMPLE CODE FOR INNER JOIN
+//	public List<Employee> findAllBySpecJoin(String firstName, int id){
+//		List<EmployeeDto> empDtoList = new ArrayList<EmployeeDto>();
+//		List<Employee> empList = new ArrayList<Employee>();
+//
+//		List<Specifications> specifications = new ArrayList<Specifications>();
+//		 
+//		employeeDAO.findAll(Specification.where((root, query, builder)->{
+//			
+//			final Join<Employee, EmployeeDetailEntity> empJoin = querySpecification.makeJoin(root, new Employee(), new EmployeeDetailEntity(), "id", JoinType.INNER);
+//			final Join<EmployeeDetailEntity, EmployeeDataEntity> empDataJoin = querySpecification.makeJoin(root, new EmployeeDetailEntity(), new EmployeeDataEntity(), "empid", JoinType.INNER);
+//
+//			if(id != 0) {
+//				specifications.add(new Specifications("id", id , "=", root, "or"));
+//			}
+//			if(firstName != null) {
+//				specifications.add(new Specifications("firstName", "%"+firstName , "%", root, "or"));
+//			}
+//			
+//			specifications.add(new Specifications("code", businessType, "=", empJoin));
+//			query.orderBy(builder.desc(root.get("id"))); //asc or desc based on id if required
+//			return builder.or(querySpecification.writeJRootsORCondition(specifications, builder));
+//
+//		})).forEach((emp)->{
+//			empList.add(emp);
+//		});
+//		
+//		return empList;		
+//	}
 }
